389cea21874a7ac79c7537f7a979d3ffdd10bd45
thanhnt <tatthanh.dev@gmail.com>
2017-03-01 16:30:13 +0700

    Code style for Presenter

--- app/Presenters/UserNotificationPresenter.php
+++ app/Presenters/UserNotificationPresenter.php
@@ -6,12 +6,12 @@ class UserNotificationPresenter extends BasePresenter
 {
     public function userName()
     {
-        if ($this->entity->user_id == 0) {
+        if( $this->entity->user_id == 0 ) {
             return 'Broadcast';
         }
 
         $user = $this->entity->user;
-        if (empty($user)) {
+        if( empty( $user ) ) {
             return 'Unknown';
         }
 
